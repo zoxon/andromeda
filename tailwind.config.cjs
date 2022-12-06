e5f59e7e8e2b571124e82f393e6d57b0c27f40dd
KV <3866196+zoxon@users.noreply.github.com>
2022-12-06 17:03:16 +0600

    Update layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        overlays: '1800',
+        modal: '1700',
+        header: '1600',
+        drawer: '1500',
+        backdrop: '1400',
+        fixed: '1300',
+        sticky: '1200',
+        dropdown: '1100',
+        tooltip: '1000',
+        max: '9999',
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
