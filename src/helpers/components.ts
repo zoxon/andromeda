40c7528de722981b7bd3f4a48a3b49e39b0115b6
KV <3866196+zoxon@users.noreply.github.com>
2022-12-30 21:21:41 +0600

    Update refs selection code

--- src/helpers/components.ts
+++ src/helpers/components.ts
@@ -3,21 +3,29 @@ import { EventBus } from '@/helpers/eventbus'
 
 const events = new EventBus()
 
-type ReferenceElement = HTMLElement | null | undefined
+const GLOBAL_REF_PREFIX = 'global'
+
+export type References = {
+  [componentName: string]: {
+    [referenceName: string]: HTMLElement[]
+  }
+}
 
 export class Component {
   name: string
   element: HTMLElement
-  refs: Record<string, ReferenceElement>
+  _refs: References = {}
   options: Record<string, any>
   events: EventBus
 
   constructor(name: string, element: HTMLElement, options: Record<string, any> = {}) {
     this.name = name
     this.element = element
-    this.refs = {}
     this.options = Object.assign({}, this.defaults, options)
     this.events = events
+
+    this._refs[name] = {}
+    this._refs[GLOBAL_REF_PREFIX] = {}
 
     if (!this.isInit()) {
       this._init()
@@ -25,7 +33,7 @@ export class Component {
   }
 
   private _init() {
-    this.initRefs()
+    this.collectRefs()
     this.buildCache()
     this.bindEvents()
     this.setInit()
@@ -44,29 +52,45 @@ export class Component {
     return this.element.dataset[`${camelCase(this.name)}Inited`] || false
   }
 
-  initRefs() {
-    const allReferencesElements = this.element.querySelectorAll<HTMLElement>('[data-ref]')
-    const elements = [this.element, ...allReferencesElements]
+  collectRefs() {
+    const referenceElements = [this.element, ...this.element.querySelectorAll<HTMLElement>('[data-ref]')]
 
-    for (const element of elements) {
-      const referenceName = element.dataset.ref
-      const isPrefixedReference = referenceName?.includes(':')
+    for (const element of referenceElements) {
+      const reference = element.dataset.ref
+      if (!reference) continue
 
-      if (!referenceName) continue
+      // eslint-disable-next-line prefer-const
+      let [componentName, referenceName] = reference.split(':')
 
-      // Support ref prefix like data-ref="component-name:ref-name"
-      if (isPrefixedReference) {
-        const [prefix, name] = referenceName.split(':')
-
-        if (prefix == this.name) {
-          this.refs[camelCase(name)] = element
-        }
-        continue
+      // Global ref
+      if (referenceName === undefined) {
+        componentName = GLOBAL_REF_PREFIX
+        referenceName = reference
       }
 
-      // Support global refs data-ref="global-ref-name"
-      this.refs[camelCase(referenceName)] = element
+      componentName = camelCase(componentName)
+      referenceName = camelCase(referenceName)
+
+      if (this._refs[componentName][referenceName] === undefined) {
+        this._refs[componentName][referenceName] = [element]
+      } else {
+        this._refs[componentName][referenceName].push(element)
+      }
     }
+  }
+
+  get refs() {
+    const references: { [referenceName: string]: HTMLElement[] } = {
+      ...this._refs[this.name],
+      ...this._refs[GLOBAL_REF_PREFIX],
+    }
+
+    const newReferences: { [referenceName: string]: HTMLElement | HTMLElement[] } = {}
+    for (const [key, value] of Object.entries(references)) {
+      newReferences[key] = value.length === 1 ? value[0] : value
+    }
+
+    return newReferences
   }
 
   init() {
@@ -89,6 +113,7 @@ export const initComponent = (name: string, ComponentClass: typeof Component, op
 
   for (const element of elements) {
     const instance = new ComponentClass(name, element, options)
+    console.log(instance)
     instances.push(instance)
   }
 }
