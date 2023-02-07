5b4ad7b065586807244cd85d0ac94b5ce3595f96
KV <3866196+zoxon@users.noreply.github.com>
2023-02-07 21:55:54 +0600

    Added drawer

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,4 +4,7 @@ declare interface WindowEventMap {
   inited: CustomEvent<string>
   'modal:show': CustomEvent<string>
   'modal:close': CustomEvent
+  'drawer:toggle': CustomEvent<string>
+  'drawer:collapse': CustomEvent<string>
+  'drawer:expand': CustomEvent<string>
 }
